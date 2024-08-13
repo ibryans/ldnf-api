039aa10053be06be3390b36d7e130f00c0785997
Bryan Oliveira <bsoliveira@proton.me>
2024-08-13 15:31:29 -0300

    Initial Java Structure

---
+++ src/main/java/com/ibryans/ldnf_api/LdnfApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibryans.ldnf_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdnfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdnfApiApplication.class, args);
+	}
+
+}
